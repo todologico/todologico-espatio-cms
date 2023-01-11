8a31d9276b7ddf4fc1edefa5b77d7d9bbbd4f699
Rendo <arturodiehl@gmail.com>
2023-01-11 15:03:53 -0300

    more text

--- demoespatio/resources/views/ccc_cont1/ccc_cont1_list.blade.php
+++ demoespatio/resources/views/ccc_cont1/ccc_cont1_list.blade.php
@@ -81,7 +81,7 @@
 
                 <tr>
                   <td style="<?=$bgcolor;?>"></td>                  
-                  <td colspan="5" style="<?=$bgcolor1;?>">MESSAGE: {{$reg->ccc_cont1_text1}}</td> 
+                  <td colspan="6" style="<?=$bgcolor1;?>">MESSAGE: {{$reg->ccc_cont1_text1}}</td> 
                 </tr>
 
                   @if($loop->last)
