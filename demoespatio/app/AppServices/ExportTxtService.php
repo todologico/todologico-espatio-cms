2658a4344f3c548595309ab808748e5d4d19cfe4
Rendo <arturodiehl@gmail.com>
2023-02-17 17:48:41 -0300

    Service Interface starter

---
+++ demoespatio/app/AppServices/ExportTxtService.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AppServices;
+
+Class ExportTxtService {
+
+
+/**
+ * Export array to Excel
+ * @param array
+ *
+ */
+
+    public function exportArray(){
+
+
+    $service ='array to excel';
+
+
+    return $service;
+
+
+    }
+
+
+}
