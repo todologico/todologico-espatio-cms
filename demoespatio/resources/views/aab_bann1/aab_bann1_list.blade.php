01785ef31e345255e5aa3cd02f6191e2c48315dd
todologico <arturodiehl@gmail.com>
2023-07-31 20:44:37 -0300

    vuejs

--- demoespatio/resources/views/aab_bann1/aab_bann1_list.blade.php
+++ demoespatio/resources/views/aab_bann1/aab_bann1_list.blade.php
@@ -90,9 +90,11 @@
                     <?if($reg->aab_bann1_enable=='1'){?>dfdfd
 
 
-                      <button type="button" id="butt1on{{$reg->aab_bann1_id}}" v-cloak :id="butt1on{{$reg->aab_bann1_id}}" v-show="true"   @click="ShowHideBannersAR({{$reg->aab_bann1_id}},'{{$reg->aab_bann1_token}}','1')" class="btn btn-success btn-sm"><i class="fa fa-power-off" aria-hidden="true"></i> ON222</button>
+                    
                       
-                      <button type="button" id="butt2on{{$reg->aab_bann1_id}}" v-cloak :id="butt2on{{$reg->aab_bann1_id}}" v-show="false"  @click="ShowHideBannersAR({{$reg->aab_bann1_id}},'{{$reg->aab_bann1_token}}','2')"  class="btn btn-danger btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> OFF111</button>
+                      <button type="button" v-cloak :id="butt2on{{$reg->aab_bann1_id}}" v-if="butt2on{{$reg->aab_bann1_id}}" @click="ShowHideBannersAR({{$reg->aab_bann1_id}},'{{$reg->aab_bann1_token}}','2')"  class="btn btn-danger btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> OFF111</button>
+
+                      <button type="button" v-cloak :id="butt1on{{$reg->aab_bann1_id}}" v-else="butt1on{{$reg->aab_bann1_id}}" @click="ShowHideBannersAR({{$reg->aab_bann1_id}},'{{$reg->aab_bann1_token}}','1')" class="btn btn-success btn-sm"><i class="fa fa-power-off" aria-hidden="true"></i> ON222</button>
 
                       <?} else {?> 
 
